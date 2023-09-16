4f634ef6edf0e8130a0d92ade35508875c906cf9
AndiyScripts <126016466+AndiyScripts@users.noreply.github.com>
2023-09-16 13:14:36 +0200

    1.01

--- client.lua
+++ client.lua
@@ -3,30 +3,34 @@ AddEventHandler('AndiyScripts_GetPos:GetLocationOfPositionAndCopyNotUseMenu', fu
   local plyPos = GetEntityCoords(cache.ped)
   local plyHead = GetEntityHeading(cache.ped)
   local DoneText = ''
+  local xpos = string.format("%.2f", plyPos.x)
+  local ypos = string.format("%.2f", plyPos.y)
+  local zpos = string.format("%.2f", plyPos.z)
+  local phead = string.format("%.2f", plyHead)
   if data.type == 'v1' or data == 'v1' then
-    DoneText = "vector3("..plyPos.x..", "..plyPos.y..", "..plyPos.z..")"
+    DoneText = "vector3("..xpos..", "..ypos..", "..zpos..")"
   elseif data.type == 'v2' or data == 'v2' then
-    DoneText = "vector4("..plyPos.x..", "..plyPos.y..", "..plyPos.z..", "..plyHead..")"
+    DoneText = "vector4("..xpos..", "..ypos..", "..zpos..", "..phead..")"
   elseif data.type == 'b1' or data == 'b1' then
-    DoneText = "{x= "..plyPos.x..", y= "..plyPos.y..", z= "..plyPos.z.."}"
+    DoneText = "{x= "..xpos..", y= "..ypos..", z= "..zpos.."}"
   elseif data.type == 'b2' or data == 'b2' then
-    DoneText = "{x= "..plyPos.x..", y= "..plyPos.y..", z= "..plyPos.z..", h= "..plyHead.."}"
+    DoneText = "{x= "..xpos..", y= "..ypos..", z= "..zpos..", h= "..phead.."}"
   elseif data.type == 't1' or data == 't1' then
-    DoneText = "x= "..plyPos.x..", y= "..plyPos.y..", z= "..plyPos.z..""
+    DoneText = "x= "..xpos..", y= "..ypos..", z= "..zpos..""
   elseif data.type == 't2' or data == 't2' then
-    DoneText = "x= "..plyPos.x..", y= "..plyPos.y..", z= "..plyPos.z..", h= "..plyHead..""
+    DoneText = "x= "..xpos..", y= "..ypos..", z= "..zpos..", h= "..phead..""
   elseif data.type == 's1' or data == 's1' then
-    DoneText = "['x'] ="..plyPos.x..", ['y'] ="..plyPos.y..", ['z'] ="..plyPos.z..""
+    DoneText = "['x'] ="..xpos..", ['y'] ="..ypos..", ['z'] ="..zpos..""
   elseif data.type == 's2' or data == 's2' then
-    DoneText = "['x'] ="..plyPos.x..", ['y'] ="..plyPos.y..", ['z'] ="..plyPos.z..", ['h'] ="..plyHead..""
+    DoneText = "['x'] ="..xpos..", ['y'] ="..ypos..", ['z'] ="..zpos..", ['h'] ="..phead..""
   elseif data.type == '1' or data == '1' then
-    DoneText = ""..plyPos.x..", "..plyPos.y..", "..plyPos.z..""
+    DoneText = ""..xpos..", "..ypos..", "..zpos..""
   elseif data.type == '2' or data == '2' then
-    DoneText = ""..plyPos.x..", "..plyPos.y..", "..plyPos.z..", "..plyHead..""
+    DoneText = ""..xpos..", "..ypos..", "..zpos..", "..phead..""
   elseif data.type == '3' or data == '3' then
-    DoneText = ""..plyPos.x..", "..plyPos.y..""
+    DoneText = ""..xpos..", "..ypos..""
   elseif data.type == '4' or data == '4' then
-    DoneText = ""..plyHead..""
+    DoneText = ""..phead..""
   end
 
   if DoneText ~= '' then
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,8 @@ game 'gta5'
 author 'AndiyScripts'
 lua54 'yes'
 
+version '1.0.1'
+
 shared_script {'config.lua', '@ox_lib/init.lua'}
 
 client_script 'client.lua'
